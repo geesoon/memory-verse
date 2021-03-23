6c8fb3e92fe0f2cfff721dfe26e68b6366f288f6
geesoon <hiewgs@gmail.com>
2021-03-23 14:44:19 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app" class="container">
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  height: 100vh;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="panel-container">
+    <h1>Choose Your Verses</h1>
+    <verses-selection-panel></verses-selection-panel>
+  </div>
+</template>
+
+<script>
+import VersesSelectionPanel from "../components/selectionPanelButtons.vue";
+export default {
+  components: {
+    VersesSelectionPanel,
+  },
+};
+</script>
+
+<style>
+.panel-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
